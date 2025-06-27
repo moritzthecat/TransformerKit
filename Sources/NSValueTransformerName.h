dc1277343b7404e4a6263406be4e4a4872dcdbf0
moritzthecat <3109519+moritzthecat@users.noreply.github.com>
2025-06-27 17:03:27 +0200

    Update NSValueTransformerName.h
    comment out @import Darwin.Availability as not needed and results in build errors

--- Sources/NSValueTransformerName.h
+++ Sources/NSValueTransformerName.h
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-@import Darwin.Availability;
+// @import Darwin.Availability;
 
 #ifndef NSValueTransformerName_h
 #define NSValueTransformerName_h
